a080e95f95bd4188359457058cdf69cc97adde9f
Gabriel Sales <gabrieldesantanasales@gmail.com>
2022-10-07 00:30:57 -0300

    Testes

---
+++ Primeiro Projeto/src/TestaCondicional.java
@@ -0,0 +1,11 @@
+public class TestaCondicional {
+    public static void main(String[] args) {
+        
+        String casa = "87";
+        String casa2= "56";
+        String total = casa + casa2;
+        
+        
+        System.out.println(total);
+    }
+}
