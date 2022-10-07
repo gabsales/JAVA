317e2db22b5c71c6cf310404d126bf082316f87c
Gabriel Sales <gabrieldesantanasales@gmail.com>
2022-10-07 03:43:07 -0300

    TesteCondicional

--- Primeiro Projeto/src/TestaCondicional.java
+++ Primeiro Projeto/src/TestaCondicional.java
@@ -1,11 +1,22 @@
 public class TestaCondicional {
+
     public static void main(String[] args) {
-        
-        String casa = "87";
-        String casa2= "56";
-        String total = casa + casa2;
-        
-        
-        System.out.println(total);
+        System.out.println("testando condicionais");
+
+        int idade = 20;
+        int quantidadePessoas = 3;
+
+        if (idade >= 18) {
+
+            System.out.println("Você tem mais que 18 anos");
+            System.out.println("Seja bem vindo");
+        } else {
+            if (quantidadePessoas >= 2) {
+                System.out.println("voce nao tem 18, mas pode entrar, pois está acompanhado");
+            } else {
+
+                System.out.println("infelizmente voce nao pode entrar");
+            }
+        }
     }
 }
